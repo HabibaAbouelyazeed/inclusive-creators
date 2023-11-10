8032960d30764908fd218d7002a70212fce0d167
Habiba Mahmoud <habibaabouelyazeed@gmail.com>
2023-11-10 21:10:59 +0200

    NavBar update + Landing: HeroSection

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = withMT({
   darkMode: "media",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+    },
     colors: {
       transparent: "transparent",
       current: "currentColor",
