396592e3ec3a0e2ac9dab770a3ff27f4ea74c443
NourhanAshraf <nourhanaashraf16@gmail.com>
2023-11-13 12:29:53 +0200

    editArticlesPage

--- src/components/articles/ArticleCard.jsx
+++ src/components/articles/ArticleCard.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import articleImage from "../../assets/images/31343C.svg";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const ArticleCard = ({ article }) => {
+  const arrowIcon = <FontAwesomeIcon icon={faArrowRight} />;
   return (
     <div className="grid grid-cols-12 my-8 shadow-lg rounded">
       <figure className="col-span-full md:col-span-4 p-4 md:p-8">
@@ -15,13 +19,19 @@ const ArticleCard = ({ article }) => {
           {article.content}
         </p>
         <div className="flex items-center">
-          <h4 className="text-dirtyPink text-sm md:text-base lg:text-lg">
+          <h4 className="text-dirtyPink text-sm md:text-base lg:text-lg font-semibold">
             {article.author}
           </h4>
-          <h5 className="text-dirtyPink text-sm md:text-base lg:text-lg mx-4 md:mx-8">
+          <h5 className="text-dirtyPink text-sm md:text-base lg:text-lg mx-4 md:mx-8 font-semibold">
             {article.date}
           </h5>
         </div>
+        <Link
+          to=""
+          className="text-olive text-sm md:text-base lg:text-lg font-medium inline-block my-2"
+        >
+          Learn more <span className="mx-1 text-base">{arrowIcon}</span>
+        </Link>
       </div>
     </div>
   );
