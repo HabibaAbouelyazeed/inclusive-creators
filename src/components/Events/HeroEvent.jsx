d606438a2bdf541fc96dcdb77630b28d6c8ece96
EsraaSamyZ <142722769+EsraaSamyZ@users.noreply.github.com>
2023-11-15 09:03:12 +0200

    fix

--- src/components/Events/HeroEvent.jsx
+++ src/components/Events/HeroEvent.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 
 const HeroEvent = ({ event }) => {
-  console.log(event);
+  // console.log(event);
 
   if (!event) {
     // Handle the case when event is not provided
